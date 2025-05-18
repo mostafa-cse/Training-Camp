e75abdf2f88c97806ec5a0fef307ff4c8427d3ef
Mostafa Kamal <100847323+mostafa-cse@users.noreply.github.com>
2025-05-18 22:09:36 +0600

    Update F - February 29.cpp

--- Contest - 03/F - February 29.cpp
+++ Contest - 03/F - February 29.cpp
@@ -2,15 +2,6 @@
 #define int long long
 using namespace std;
 char coma;
-int Month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-string Month_Name[] = {"", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-map<string, int> getMonth;
-int check(int n) {
-    if (n % 400 == 0) return true;
-    if (n % 100 == 0) return false;
-    if (n % 4 == 0) return true;
-    return false;
-}
 void solve() {
     string month_birth;
     int date_birth, year_birth;
@@ -20,11 +11,9 @@ void solve() {
     int date_cur, year_cur;
     cin >> cur_month >> date_cur >> coma >> year_cur;
 
-    if (getMonth[month_birth] > 2)  year_birth++;
-    while (!check(year_birth)) year_birth++;
+    if (month_birth != "January" and month_birth != "February")  year_birth++;
     
-    if (getMonth[cur_month] == 1 or getMonth[cur_month] == 2 and date_cur < 29) year_cur--;
-    while(!check(year_cur)) year_cur--;
+    if (cur_month == "January" or cur_month == "February" and date_cur < 29) year_cur--;
 
     if (year_birth > year_cur) {
         cout << 0 << endl;
@@ -36,9 +25,6 @@ void solve() {
     }
 }
 signed main() {
-    for (int i = 1; i <= 12; i++) {
-        getMonth[Month_Name[i]] = i;
-    }
     int tc;
     cin >> tc;
 
