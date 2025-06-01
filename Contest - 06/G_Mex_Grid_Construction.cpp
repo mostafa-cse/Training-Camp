01c23b75934147b76bb4a3d754923f95bda71c5a
Mostafa Kamal <100847323+mostafa-cse@users.noreply.github.com>
2025-06-02 00:07:19 +0600

    Update G_Mex_Grid_Construction.cpp

--- Contest - 06/G_Mex_Grid_Construction.cpp
+++ Contest - 06/G_Mex_Grid_Construction.cpp
@@ -1,36 +1,31 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
+ 
 signed main() {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> a(n, vector<int>(m, -1));
+    int n;
+    cin >> n;
+ 
+    vector<vector<int>> a(n, vector<int>(n, n * n));
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            char c;
-            cin >> c;
-            a[i][j] = (int)(c - 'A');
-        }
-    }
-    vector<vector<int>> ans(n, vector<int>(m, -1));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            for (auto el : {0, 1, 2, 3}) {
-                if (el != a[i][j]) {
-                    bool ok = true;
-                    if (j) ok &= (ans[i][j - 1] != el);
-                    if (j + 1 < m) ok &= (ans[i][j + 1] != el);
-                    if (i) ok &= (ans[i - 1][j] != el);
-                    if (i + 1 < n) ok &= (ans[i + 1][j] != el);
-                    if (ok) {
-                        ans[i][j] = el;
-                        break;
-                    }
+        for (int j = 0; j < n; j++) {
+            vector<int> hsh(n * n + 1, 0);
+            for (int r = 0; r < i; r++) hsh[a[r][j]] = 1;
+            for (int c = 0; c < j; c++) hsh[a[i][c]] = 1;
+ 
+            for (int val = 0; val < n * n; val++) {
+                if (hsh[val] == 0) {
+                    a[i][j] = val;
+                    break;
                 }
             }
-            cout << char(ans[i][j] + 'A');
         }
-        cout << endl;
+    }
+ 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << a[i][j] << " \n"[j + 1 == n];
+        }
     }
     return 0;
 }
